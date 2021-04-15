438be7221edcce4cdd451da988780e74c9b955d2
wasted-soulless <ayubzakirtushar@gmail.com>
2021-04-15 14:44:54 +0200

    Updated penalty handler

--- src/main/java/io/swcode/teams/bot/mrkrabs/PenaltyHandler.java
+++ src/main/java/io/swcode/teams/bot/mrkrabs/PenaltyHandler.java
@@ -1,5 +1,7 @@
 package io.swcode.teams.bot.mrkrabs;
 
+import java.sql.*;
+
 public class PenaltyHandler implements DatabaseReportInterface{
     private String user;
     private String type;
@@ -17,7 +19,26 @@ public class PenaltyHandler implements DatabaseReportInterface{
 
 
     @Override
-    public void executeQuery(String tableRow, String tableName) {
+    public void executeQuery(String tableRow, String tableName){
+        try
+        {
+            Connection connection = null;
+            Class.forName("org.postgresql.Driver");
+            connection = DriverManager.getConnection(url, dbUser, passWord);
+            System.out.println("Connected to PostgreSQL database!");
+            Statement statement = connection.createStatement();
+            ResultSet commandVal = statement.executeQuery("UPDATE penalty_user SET penalty_amount = 0 WHERE user_name LIKE 'zakir@swcode.io'");
+            while (commandVal.next())
+            {
+
+            }
+            statement.close();
+            commandVal.close();
+        }
+        catch (SQLException | ClassNotFoundException e) {
+            System.out.println("Connection failure.");
+            e.printStackTrace();
+        }
 
     }
 
